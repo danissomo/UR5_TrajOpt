195a9a4266565ae8bb2570542351f0822aa0d1c0
Elena <ellena.hella@yandex.ru>
2022-11-28 01:24:59 +0300

    Перенос наработок.

---
+++ src/UR5/ur5_single_arm_manipulation/scripts/run.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import rospy
+import moveit_commander
+import tf
+import rospkg
+import os
+
+from gazebo_msgs.srv import (
+    SpawnModel,
+    DeleteModel,
+)
+
+from geometry_msgs.msg import (
+    PoseStamped,
+    Pose,
+    Point,
+    Quaternion,
+)
+
