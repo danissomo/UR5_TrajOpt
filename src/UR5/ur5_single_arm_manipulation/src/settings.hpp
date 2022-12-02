15cf55270273ae65cff9f678fc6d6f4ffd9407a5
Elena <ellena.hella@yandex.ru>
2022-12-02 18:40:54 +0300

    Доработка двери, которую можно открыть и закрыть полностью. Добавлены настройки.

--- src/UR5/ur5_single_arm_manipulation/src/settings.hpp
+++ src/UR5/ur5_single_arm_manipulation/src/settings.hpp
@@ -3,9 +3,11 @@
 #ifndef settings
 #define settings
 
-float handlePosition_x = -0.145;
-float handlePosition_y = 0.4;
-float handlePosition_z = 1.05;
+float handlePosition_x = -0.3;
+float handlePosition_y = 0.45;
+float handlePosition_z = 1.095;
+int doorHeight = ;
+int handleDistanceFromEdge = ;
 
 float gripperOpen = 0;
 float gripperPickHandle = 0.56;
