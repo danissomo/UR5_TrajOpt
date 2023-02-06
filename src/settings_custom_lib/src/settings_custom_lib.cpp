f8ab27bfb72125485971bbc4576b039098ddfc4b
Elena <ellena.hella@yandex.ru>
2023-02-07 00:20:13 +0300

    Попытка вызвать обновление настроек из другого пакета.

--- src/settings_custom_lib/src/settings_custom_lib.cpp
+++ src/settings_custom_lib/src/settings_custom_lib.cpp
@@ -1,1 +1,19 @@
 #include "settings_custom_lib/SettingsCustomLib.hpp"
+
+SettingsCustomLibClass settingsConfig;
+
+int main(int argc, char *argv[]) {
+
+	ROS_INFO("Start update settings....:");
+    ros::init(argc, argv, "settings_custom_lib");
+
+    ros::NodeHandle nh;
+    ros::AsyncSpinner spinner(10);
+    spinner.start();
+
+    settingsConfig.update();
+    ros::Duration(1).sleep();
+    ros::waitForShutdown();
+
+    return 0;
+}
