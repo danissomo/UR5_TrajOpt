49a03f3af39fd1402d21efc047617518a32cd94f
Elena <ellena.hella@yandex.ru>
2023-05-22 18:51:33 +0300

    Мелкие правки.

--- src/ur5_husky_main/ik/InverseKinematics.cpp
+++ src/ur5_husky_main/ik/InverseKinematics.cpp
@@ -6,9 +6,9 @@
 
 
 InverseKinematics::InverseKinematics(double x, double y, double z) {
-	x_ = x;
-	y_ = y;
-	z_ = z;
+    x_ = x;
+    y_ = y;
+    z_ = z;
 }
 
 std::vector<double> InverseKinematics::caclulate() {
