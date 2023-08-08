bdfec0b8df3405aca71cb6686eb850a9eb7140b3
Muhammad Alhaddad <adeshkin.vi@phystech.edu>
2023-08-08 17:18:56 +0300

    Мелкие правки

--- src/ur5_husky_api/pkgs_for_datasets/arm_msgs/scripts/manipulator_state.py
+++ src/ur5_husky_api/pkgs_for_datasets/arm_msgs/scripts/manipulator_state.py
@@ -4,6 +4,8 @@
 ##                                                                              ##
 ##  Information about the status of the manipulator (arm_msgs/ManipulatorState) ##
 ##                                                                              ##
+##             ATTENTION! See example in the ur5_info package                   ##
+##                                                                              ##
 ##################################################################################
 
 
@@ -43,47 +45,11 @@ class Robot():
         self.state_pub = rospy.Publisher('/state/arm/0/arm_state', ManipulatorState, queue_size=10)
         rospy.on_shutdown(self.shutdown)
 
-    def state(self):
-        msg = ManipulatorState()
-        msg.q_target = self._rtde_r.getTargetQ()
-        msg.qd_target = self._rtde_r.getTargetQd()
-        msg.i_target = self._rtde_r.getTargetCurrent()
-        msg.m_target = self._rtde_r.getTargetMoment()
-        msg.tau_target = self._rtde_r.getTargetMoment()
-        msg.tool_vector_target = self._rtde_r.getTargetTCPPose()
-        msg.q_actual = self._rtde_r.getActualQ()
-        msg.qd_actual = self._rtde_r.getActualQd()
-        msg.i_actual = self._rtde_r.getActualCurrent()
-        msg.tau_actual = self.tau_actual
-        msg.tcp_force = self._rtde_r.getActualTCPForce()
-        msg.tool_vector_actual = self._rtde_r.getActualTCPPose()
-        msg.tcp_speed = self._rtde_r.getActualTCPSpeed()
-        msg.motor_temperatures = self._rtde_r.getJointTemperatures()
-        msg.joint_modes =  [float(item) for item in self._rtde_r.getJointMode()]
-        msg.controller_timer = self._rtde_r.getActualExecutionTime()
-        msg.qdd_target = self._rtde_r.getTargetQdd()
-        msg.qdd_actual = []
-        msg.tool_acc_values = self._rtde_r.getActualToolAccelerometer()
-        msg.robot_mode = self._rtde_r.getRobotMode()
-        msg.digital_input_bits = float(self._rtde_r.getActualDigitalInputBits())
-        msg.test_value = 0.0
-
-        self.state_pub.publish(msg)
-
-        self.rate.sleep()
-        # self._rtde_c.stopScript()
-        # self._rtde_c.disconnect()
-        
-    def getActualJointTorques(self):
-        self.tau_actual = self._rtde_c.getJointTorques()
-
     def shutdown(self):
         rospy.sleep(1)
 
     def spin(self):
         while not rospy.is_shutdown():
-            self.getActualJointTorques()
-            self.state()
             self.rate.sleep()
 
 robot = Robot()
